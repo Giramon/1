87b09297c60abfa71c071f6e02de40e00878dc62
Giramon <120495791+Giramon@users.noreply.github.com>
2023-05-03 20:44:32 +0500

    Update database.js

--- app/database/database.js
+++ app/database/database.js
@@ -1,1 +1,28 @@
+var sqlite3 = require('sqlite3').verbose()
+var md5 = require('md5')
 
+const DBSOURCE = "db.sqlite"
+
+let db = new sqlite3.Database(DBSOURCE, (err) => {
+    if (err) {
+      // Cannot open database
+      console.error(err.message)
+      throw err
+    }else{
+        console.log('Connected to the SQLite database.')
+        db.run(`CREATE TABLE user (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            author text,
+            gold number,
+            message number,   
+            )`,
+        (err) => {
+            if (err) {
+                // Table already created
+            }
+        });  
+    }
+});
+
+
+module.exports = db
