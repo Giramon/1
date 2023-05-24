aa6f00dc544f3cdc43a9c8bd0ad93869e93056eb
Giramon <120495791+Giramon@users.noreply.github.com>
2023-05-24 18:30:02 +0500

    db новое

--- app/database/database.js
+++ app/database/database.js
@@ -1,8 +1,9 @@
-var sqlite3 = require('sqlite3').verbose()
-var md5 = require('md5')
+const express = require("express")
+const  app = express()
+const sqlite3 = require('sqlite3').verbose()
+const md5 = require('md5')
 
 const DBSOURCE = "db.sqlite"
-
 let db = new sqlite3.Database(DBSOURCE, (err) => {
     if (err) {
       // Cannot open database
@@ -12,17 +13,50 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
         console.log('Connected to the SQLite database.')
         db.run(`CREATE TABLE user (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            author text,
+            name TEXT,
             gold number,
-            message number,   
-            )`,
+            message number)`,
+       
         (err) => {
             if (err) {
                 // Table already created
+                console.log('BD created');
             }
         });  
     }
 });
+app.post("/", (req, res, next) => {
+    var errors=[]
+    if (!req.body.password){
+        errors.push("No  gold specified");
+    }
+    if (!req.body.email){
+        errors.push("No message specified");
+    }
+    if (errors.length){
+        res.status(400).json({"error":errors.join(",")});
+        return;
+    }
+    var data = {
+        name: req.body.name,
+        email: req.body.email,
+        password : md5(req.body.password)
+    }
+    var sql ='INSERT INTO user (name, gold, message) VALUES (?,?,?)'
+    var params =[data.name, data.email, data.password]
+    db.run(sql, params, function (err, result) {
+        if (err){
+            res.status(400).json({"error": err.message})
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": data,
+            "id" : this.lastID
+        })
+    });
+})
+
 
 
 module.exports = db
