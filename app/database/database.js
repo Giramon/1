4cd7290961e68d710640ea979b7fb8fc00fa7eef
Giramon <120495791+Giramon@users.noreply.github.com>
2023-05-31 21:22:40 +0500

    db

--- app/database/database.js
+++ app/database/database.js
@@ -1,7 +1,7 @@
-const express = require("express")
-const  app = express()
-const sqlite3 = require('sqlite3').verbose()
-const md5 = require('md5')
+import express from "express";
+const  app = express();
+import sqlite3  from "sqlite3";
+import md5 from "md5";
 
 const DBSOURCE = "db.sqlite"
 let db = new sqlite3.Database(DBSOURCE, (err) => {
@@ -25,38 +25,5 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
         });  
     }
 });
-app.post("/", (req, res, next) => {
-    var errors=[]
-    if (!req.body.password){
-        errors.push("No  gold specified");
-    }
-    if (!req.body.email){
-        errors.push("No message specified");
-    }
-    if (errors.length){
-        res.status(400).json({"error":errors.join(",")});
-        return;
-    }
-    var data = {
-        name: req.body.name,
-        email: req.body.email,
-        password : md5(req.body.password)
-    }
-    var sql ='INSERT INTO user (name, gold, message) VALUES (?,?,?)'
-    var params =[data.name, data.email, data.password]
-    db.run(sql, params, function (err, result) {
-        if (err){
-            res.status(400).json({"error": err.message})
-            return;
-        }
-        res.json({
-            "message": "success",
-            "data": data,
-            "id" : this.lastID
-        })
-    });
-})
-
-
 
 module.exports = db
